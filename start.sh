f047c34825f9b4cb51bcd5b68f97a9d3c3188cd7
yayanksenpai <84301128+yayanksenpai@users.noreply.github.com>
2021-08-16 18:23:39 +0700

    Load config file externally
    For CI/CD convenient

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
+if [[ -n $CONFIG_URL ]]; then
+	wget -q $CONFIG_URL -O /usr/src/app/config.env
+fi
+
 gunicorn wserver:start_server --bind 0.0.0.0:$PORT --worker-class aiohttp.GunicornWebWorker & qbittorrent-nox -d --webui-port=8090 & python3 alive.py & ./aria.sh; python3 -m bot 
