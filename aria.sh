f9b5bc6facc429bb77c0521dfdca4f261662c5bb
yayanksenpai <84301128+yayanksenpai@users.noreply.github.com>
2021-08-16 13:50:50 +0700

    Adding trackers list back

--- aria.sh
+++ aria.sh
@@ -1,6 +1,8 @@
+tracker_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt https://ngosang.github.io/trackerslist/trackers_all_http.txt https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_combine.txt | awk '$0' | tr '\n' ',')
+
 aria2c --enable-rpc --check-certificate=false \
    --max-connection-per-server=10 --rpc-max-request-size=1024M \
-   --bt-stop-timeout=1200 --min-split-size=10M --follow-torrent=mem --split=10 \
+   --bt-tracker="[$tracker_list]" --bt-stop-timeout=1200 --min-split-size=10M --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=0 \
    --max-overall-upload-limit=1K --max-concurrent-downloads=15 --continue=true \
    --peer-id-prefix=-qB4360- --user-agent=qBittorrent/4.3.6 --peer-agent=qBittorrent/4.3.6 \
